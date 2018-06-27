9f9ca37c44f9f39123653bacd9b120c1fc5cb8cd
amirotin <ai.mirotin@gmail.com>
2018-06-27 08:56:50 +0300

    Исправлена логика в работе с tmdb

--- Contents/Libraries/Shared/kinoplex/sources/tmdb.py
+++ Contents/Libraries/Shared/kinoplex/sources/tmdb.py
@@ -97,8 +97,8 @@ class TMDBSource(SourceBase):
         try:
             self.l("checking %s search vector: %s" % (search_type, url))
             res = self._fetch_xml(url)
-
-            for match in res.xpath("//match[@lang='%s']" % lang):
+            xpath = "//match[@lang='%s']" % lang if search_type=='hash' else '//match'
+            for match in res.xpath(xpath):
                 id    = "tt%s" % match.get('guid')
                 name  = self.safe_unicode(match.get('title'))
                 year  = self.safe_unicode(match.get('year'))
@@ -120,6 +120,7 @@ class TMDBSource(SourceBase):
                     vector[0] = dist
                     vector[1] = name
                     vector[2] = year
+                self.l('vector = %s', vector)
 
         except Exception, e:
             self.l("freebase/proxy %s lookup failed: %s" % (search_type, str(e)))
@@ -146,15 +147,14 @@ class TMDBSource(SourceBase):
         self.l('hash matches %s', hash_matches)
         if hash_matches:
             self.score_hash(metadata, hash_matches)
-        self.l('hash matches score %s', hash_matches)
         for key in hash_matches.keys():
             match = hash_matches[key]
             if int(match[5]) >= GOOD_SCORE:
                 return key
 
         self.get_hash_results(metadata, title_year_matches, search_type='title/year')
-        self.score_hash(metadata, title_year_matches)
         self.l('title_year_matches = %s', title_year_matches)
+        self.score_hash(metadata, title_year_matches)
         for key in title_year_matches.keys():
             match = title_year_matches[key]
             if int(match[5]) >= GOOD_SCORE:
@@ -217,95 +217,96 @@ class TMDBSource(SourceBase):
         if not source_id:
             source_id = metadata['meta_ids'][self.source_name] = self._search(metadata, media, lang)
 
-        config_dict = self._fetch_json(tmdb.config)
-        movie_data = self._fetch_json(tmdb.movie(source_id, lang))
-        if not isinstance(movie_data, dict) or 'overview' not in movie_data or movie_data['overview'] is None or movie_data['overview'] == "":
-            movie_data = self._fetch_json(tmdb.movie(source_id, ''))
+        if source_id:
+            config_dict = self._fetch_json(tmdb.config)
+            movie_data = self._fetch_json(tmdb.movie(source_id, lang))
+            if not isinstance(movie_data, dict) or 'overview' not in movie_data or movie_data['overview'] is None or movie_data['overview'] == "":
+                movie_data = self._fetch_json(tmdb.movie(source_id, ''))
 
-        if re.match('t*[0-9]{7}', str(source_id)):
-            metadata['meta_ids']['tmdb'] = movie_data.get('id')
+            if re.match('t*[0-9]{7}', str(source_id)):
+                metadata['meta_ids']['tmdb'] = movie_data.get('id')
 
-        if metadata['meta_ids'].get('imdb') is None and movie_data.get('imdb_id') is not None:
-            metadata['meta_ids']['imdb'] = movie_data.get('imdb_id')
+            if metadata['meta_ids'].get('imdb') is None and movie_data.get('imdb_id') is not None:
+                metadata['meta_ids']['imdb'] = movie_data.get('imdb_id')
 
-        # Collections.
-        metadata['collections'] = []
-        if movie_data.get('belongs_to_collection', ''):
-            metadata['collections'].append(movie_data['belongs_to_collection']['name'].replace(' Collection', ''))
+            # Collections.
+            metadata['collections'] = []
+            if movie_data.get('belongs_to_collection', ''):
+                metadata['collections'].append(movie_data['belongs_to_collection']['name'].replace(' Collection', ''))
 
-        # Studio.
-        if 'production_companies' in movie_data and len(movie_data['production_companies']) > 0:
-            index = movie_data['production_companies'][0]['id']
-            company = None
-            for studio in movie_data['production_companies']:
-                if studio['id'] <= index:
-                    index = studio['id']
-                    company = studio['name'].strip()
-            metadata['studio'] = company
-        else:
-            metadata['studio'] = None
+            # Studio.
+            if 'production_companies' in movie_data and len(movie_data['production_companies']) > 0:
+                index = movie_data['production_companies'][0]['id']
+                company = None
+                for studio in movie_data['production_companies']:
+                    if studio['id'] <= index:
+                        index = studio['id']
+                        company = studio['name'].strip()
+                metadata['studio'] = company
+            else:
+                metadata['studio'] = None
 
-        if movie_data.get('vote_count', 0) > 3:
-            metadata['tmbp_rating'] = movie_data.get('vote_average', 0)
+            if movie_data.get('vote_count', 0) > 3:
+                metadata['tmbp_rating'] = movie_data.get('vote_average', 0)
 
-        valid_names = list()
-        metadata['tmdb_posters'] = {}
-        movie_images = movie_data.get('images', {})
-        if movie_images.get('posters'):
-            max_average = max([(lambda p: p['vote_average'] or 5)(p) for p in movie_images['posters']])
-            max_count = max([(lambda p: p['vote_count'])(p) for p in movie_images['posters']]) or 1
-        
-            for i, poster in enumerate(movie_images['posters']):
-        
-                score = (poster['vote_average'] / max_average) * POSTER_SCORE_RATIO
-                score += (poster['vote_count'] / max_count) * (1 - POSTER_SCORE_RATIO)
-                movie_images['posters'][i]['score'] = score
-        
-                # Discount score for foreign posters.
-                if poster['iso_639_1'] != lang and poster['iso_639_1'] is not None and poster['iso_639_1'] != 'en':
-                    movie_images['posters'][i]['score'] = poster['score'] - 1
-        
-            for i, poster in enumerate(sorted(movie_images['posters'], key=lambda k: k['score'], reverse=True)):
-                if i > ARTWORK_ITEM_LIMIT:
-                    break
-                else:
-                    poster_url = config_dict['images']['base_url'] + 'original' + poster['file_path']
-                    thumb_url = config_dict['images']['base_url'] + 'w154' + poster['file_path']
-                    valid_names.append(poster_url)
-        
-                    if poster_url not in metadata['tmdb_posters']:
-                        try: metadata['tmdb_posters'][poster_url] = (thumb_url, i+1)
-                        except: pass
-        
-        # Backdrops.
-        valid_names = list()
-        metadata['tmdb_art'] = {}
-        if movie_images.get('backdrops'):
-            max_average = max([(lambda p: p['vote_average'] or 5)(p) for p in movie_images['backdrops']])
-            max_count = max([(lambda p: p['vote_count'])(p) for p in movie_images['backdrops']]) or 1
-        
-            for i, backdrop in enumerate(movie_images['backdrops']):
-        
-                score = (backdrop['vote_average'] / max_average) * BACKDROP_SCORE_RATIO
-                score += (backdrop['vote_count'] / max_count) * (1 - BACKDROP_SCORE_RATIO)
-                movie_images['backdrops'][i]['score'] = score
-        
-                # For backdrops, we prefer "No Language" since they're intended to sit behind text.
-                if backdrop['iso_639_1'] == 'xx' or backdrop['iso_639_1'] == 'none':
-                    movie_images['backdrops'][i]['score'] = float(backdrop['score']) + 2
+            valid_names = list()
+            metadata['tmdb_posters'] = {}
+            movie_images = movie_data.get('images', {})
+            if movie_images.get('posters'):
+                max_average = max([(lambda p: p['vote_average'] or 5)(p) for p in movie_images['posters']])
+                max_count = max([(lambda p: p['vote_count'])(p) for p in movie_images['posters']]) or 1
 
-                # Discount score for foreign art.
-                if backdrop['iso_639_1'] != lang and backdrop['iso_639_1'] is not None and backdrop['iso_639_1'] != 'en':
-                    movie_images['backdrops'][i]['score'] = float(backdrop['score']) - 1
-        
-            for i, backdrop in enumerate(sorted(movie_images['backdrops'], key=lambda k: k['score'], reverse=True)):
-                if i > ARTWORK_ITEM_LIMIT:
-                    break
-                else:
-                    backdrop_url = config_dict['images']['base_url'] + 'original' + backdrop['file_path']
-                    thumb_url = config_dict['images']['base_url'] + 'w300' + backdrop['file_path']
-                    valid_names.append(backdrop_url)
-        
-                    if backdrop_url not in metadata['tmdb_art']:
-                        try: metadata['tmdb_art'][backdrop_url] = (thumb_url, i+1)
-                        except: pass
+                for i, poster in enumerate(movie_images['posters']):
+
+                    score = (poster['vote_average'] / max_average) * POSTER_SCORE_RATIO
+                    score += (poster['vote_count'] / max_count) * (1 - POSTER_SCORE_RATIO)
+                    movie_images['posters'][i]['score'] = score
+
+                    # Discount score for foreign posters.
+                    if poster['iso_639_1'] != lang and poster['iso_639_1'] is not None and poster['iso_639_1'] != 'en':
+                        movie_images['posters'][i]['score'] = poster['score'] - 1
+
+                for i, poster in enumerate(sorted(movie_images['posters'], key=lambda k: k['score'], reverse=True)):
+                    if i > ARTWORK_ITEM_LIMIT:
+                        break
+                    else:
+                        poster_url = config_dict['images']['base_url'] + 'original' + poster['file_path']
+                        thumb_url = config_dict['images']['base_url'] + 'w154' + poster['file_path']
+                        valid_names.append(poster_url)
+
+                        if poster_url not in metadata['tmdb_posters']:
+                            try: metadata['tmdb_posters'][poster_url] = (thumb_url, i+1)
+                            except: pass
+
+            # Backdrops.
+            valid_names = list()
+            metadata['tmdb_art'] = {}
+            if movie_images.get('backdrops'):
+                max_average = max([(lambda p: p['vote_average'] or 5)(p) for p in movie_images['backdrops']])
+                max_count = max([(lambda p: p['vote_count'])(p) for p in movie_images['backdrops']]) or 1
+
+                for i, backdrop in enumerate(movie_images['backdrops']):
+
+                    score = (backdrop['vote_average'] / max_average) * BACKDROP_SCORE_RATIO
+                    score += (backdrop['vote_count'] / max_count) * (1 - BACKDROP_SCORE_RATIO)
+                    movie_images['backdrops'][i]['score'] = score
+
+                    # For backdrops, we prefer "No Language" since they're intended to sit behind text.
+                    if backdrop['iso_639_1'] == 'xx' or backdrop['iso_639_1'] == 'none':
+                        movie_images['backdrops'][i]['score'] = float(backdrop['score']) + 2
+
+                    # Discount score for foreign art.
+                    if backdrop['iso_639_1'] != lang and backdrop['iso_639_1'] is not None and backdrop['iso_639_1'] != 'en':
+                        movie_images['backdrops'][i]['score'] = float(backdrop['score']) - 1
+
+                for i, backdrop in enumerate(sorted(movie_images['backdrops'], key=lambda k: k['score'], reverse=True)):
+                    if i > ARTWORK_ITEM_LIMIT:
+                        break
+                    else:
+                        backdrop_url = config_dict['images']['base_url'] + 'original' + backdrop['file_path']
+                        thumb_url = config_dict['images']['base_url'] + 'w300' + backdrop['file_path']
+                        valid_names.append(backdrop_url)
+
+                        if backdrop_url not in metadata['tmdb_art']:
+                            try: metadata['tmdb_art'][backdrop_url] = (thumb_url, i+1)
+                            except: pass

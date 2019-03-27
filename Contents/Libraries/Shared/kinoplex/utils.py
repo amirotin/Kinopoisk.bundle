ce352e55ffae00e762b0546a4b3d85cc49215605
amirotin <ai.mirotin@gmail.com>
2019-03-27 12:59:12 +0300

    Обновление версии в описании

--- Contents/Libraries/Shared/kinoplex/utils.py
+++ Contents/Libraries/Shared/kinoplex/utils.py
@@ -47,7 +47,7 @@ class PlexHTTPAdapter(HTTPAdapter):
 
 
 def getVersionInfo(core):
-    current_version = 'DEVELOP-130319'
+    current_version = 'DEVELOP-270319'
     current_mtime = 0
     version_path = core.storage.join_path(core.bundle_path, 'Contents', 'VERSION')
     if core.storage.file_exists(version_path):
